58ed7856bca4548c4b8bc18d67e8a1e63d25bf9d
Bart Roorda <bart@tuxion.nl>
2012-10-13 12:05:51 +0200

    Removed dependency to plugin 'SlickmapCSS' in module 'menu/sitemap'.

--- site/components/menu/templates/frontend/_sitemap.php
+++ site/components/menu/templates/frontend/_sitemap.php
@@ -1,18 +1,5 @@
-<?php
-echo load_plugin('SlickmapCSS');
-?>
+<?php namespace components\menu; if(!defined('TX')) die('No direct access.'); ?>
 
-<div class="mod-menu-sitemap sitemap">
-  <?php
-
-  echo $data;
-
-  ?>
+<div class="mod-menu--sitemap">
+  <?php echo $data; ?>
 </div>
-
-<style type="text/css">
-.content{
-  height:720px;
-
-}
-</style>
