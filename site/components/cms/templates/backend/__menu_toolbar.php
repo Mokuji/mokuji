598085b241ad5cad6d1f0059d1348e66bcf28ad4
Avaq <aldwin.vlasblom@gmail.com>
2012-10-31 16:20:30 +0100

    Adds the site title and site configuration button to the tool-bar.

--- site/components/cms/templates/backend/__menu_toolbar.php
+++ site/components/cms/templates/backend/__menu_toolbar.php
@@ -1,12 +1,12 @@
 <?php namespace components\cms; if(!defined('TX')) die('No direct access.');?>
 
 <menu type="toolbar" class="menu-items-toolbar">
-
-  <?php if($data->sites->size() > 1): ?>
+  
+  <!-- The set of buttons for selecting different sites. -->
+  <menu>
     
-    <!-- The set of buttons for selecting different sites. Only here when multiple sites are available. -->
-    <menu>
-    
+    <?php if($data->sites->size() > 1): ?>
+      
       <li>
         <?php echo $data->sites->as_options('site_id', 'title', 'id', array(
           'id'=>'btn-select-site',
@@ -14,38 +14,49 @@
           'default'=>$data->site_id)
         ); ?>
       </li>
-      <li><a href="<?php echo url('view=cms/sites&menu=NULL&pid=NULL'); ?>" id="btn-edit-sites"><?php __('Edit sites'); ?></a></li>
+        
+    <?php else: ?>
       
-    </menu>
+      <li><h3 class="site-title" title="<?php echo $data->sites->{0}->title; ?>"><?php echo $data->sites->{0}->title; ?></h3></li>
+      <li><a href="<?php echo url('view=cms/sites&menu=NULL&pid=NULL'); ?>" id="btn-edit-sites" title="<?php __('Manage sites'); ?>"><?php __('Manage sites'); ?></a></li>
     
-  <?php endif; ?>
+    <?php endif; ?>
+  
+  </menu>
   
   <!-- The set of buttons used to control the menu items. -->
   <menu>
-    <li><a href="<?php echo url('section=cms/app&menu=0', true); ?>" id="btn-new-menu-item"><?php __('New menu item'); ?></a></li>
+    <li><a href="<?php echo url('section=cms/app&menu=0', true); ?>" id="btn-new-menu-item" title="<?php __('New menu item'); ?>"><?php __('New menu item'); ?></a></li>
   </menu>
     
   <!-- The set of buttons used to save, refresh or revert the menu items. -->
   <menu>
   
-    <li><a href="<?php echo url('section=cms/menu_item_list'); ?>" id="btn-refresh-menu-items"><?php __('Refresh menu items'); ?></a></li>
+    <li><a href="<?php echo url('section=cms/menu_item_list'); ?>" id="btn-refresh-menu-items" title="<?php __('Refresh menu items'); ?>"><?php __('Refresh menu items'); ?></a></li>
     <li class="menu-state" id="dropdown-menu">
 
       <a href="#" id="btn-save-menu-items"><?php __('Save menu items'); ?></a>
       <a href="#" id="user-message"><?php __('Saved successfully'); ?></a>
 
       <?php
+      
       //Menu select.
-      if($data->menus->size() > 1){
+      if($data->menus->size() > 1)
+      {
+        
         echo $data->menus->as_options('menu_id', 'title', 'id', array(
           'id'=>'btn-select-menu',
           'force_choice'=>true,
           'default'=>$data->menu_id
         ));
-      }elseif($data->menus->size() == 1){
-        //If only one menu -> hide menu select box.
+        
+      }
+      
+      //If only one menu -> hide menu select box.
+      elseif($data->menus->size() == 1){
         echo '<input type="hidden" name="menu_id" value="'.$data->menus->{0}->id.'">';
       }
+      
       ?>
 
     </li>
