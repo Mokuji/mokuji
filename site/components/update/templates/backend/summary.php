963577eef333ca0d12d96353d979e045db35d773
Avaq <aldwin.vlasblom@gmail.com>
2012-11-23 19:45:19 +0100

    Bugfixes. * Fixed a bug in the update component that would cause errors in the updates page. * Fixed a bug in the Image class that would cause images to not be cached correctly after cropping.
    Conflicts:
    * site/components/update/templates/backend/summary.php
      First bugfix was already fixed in an inferiour way. Overwritten.

--- site/components/update/templates/backend/summary.php
+++ site/components/update/templates/backend/summary.php
@@ -1,27 +1,27 @@
 <?php namespace components\update; if(!defined('TX')) die('No direct access.'); tx('Account')->page_authorisation(2); ?>
 
-<h1><?php __('Update management'); ?></h1>
-<input type="button" id="check_updates" class="button grey" value="<?php __('Check for updates'); ?>" />
+<h1><?php __($names->component, 'Update management'); ?></h1>
+<input type="button" id="check_updates" class="button grey" value="<?php __($names->component, 'Check for updates'); ?>" />
 
-<h2><?php __('Recent updates'); ?></h2>
+<h2><?php __($names->component, 'Recent updates'); ?></h2>
 <div id="recent-updates">
   
-  <?php $data->latest_updates->each(function($version){ ?>
+  <?php $data->latest_updates->each(function($version)use($names){ ?>
     
     <div class="version-wrapper">
       <h4 title="<?php echo $version->package->description; ?>"><?php echo $version->package->title; ?></h4>
-      <?php __('Version'); ?>:
+      <?php __($names->component, 'Version'); ?>:
       <span class="version"><?php echo $version->version; ?></span>
       <span class="date">(<?php echo $version->date; ?>)</span>
       <div class="description"><?php echo $version->description; ?></div>
       
-      <?php $version->changes->each(function($change){ ?>
+      <?php $version->changes->each(function($change)use($names){ ?>
         
         <div class="change-wrapper">
           <h4><?php echo $change->title; ?></h4>
           <div class="change-description"><?php echo $change->description; ?></div>
-          <?php echo $change->url->not('empty', function($url){
-            return '<a href="'.$url.'" target="_blank">'.__('More information', 1).'</a>';
+          <?php echo $change->url->not('empty', function($url)use($names){
+            return '<a href="'.$url.'" target="_blank">'.__($names->component, 'More information', 1).'</a>';
           }); ?>
         </div>
         
