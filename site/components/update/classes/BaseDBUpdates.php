63b82b9eb9ac638ba8c8be4e4cc7143331ea8305
Avaq <aldwin.vlasblom@gmail.com>
2012-12-24 14:34:16 +0100

    Fixed problems in install process. * Fixed a bug in BaseDBUpdates which caused the update progress to stop and store an invalid version name when no Database Updates are defined in a version bump. * Implemented backwards compatibility for the latest update.

--- site/components/update/classes/BaseDBUpdates.php
+++ site/components/update/classes/BaseDBUpdates.php
@@ -64,7 +64,8 @@ abstract class BaseDBUpdates
   
   /* ---------- Static ---------- */
   
-  static function base_dir($type = 'core', $name = null){
+  static function base_dir($type = 'core', $name = null)
+  {
     
     switch($type){
       
@@ -372,6 +373,7 @@ abstract class BaseDBUpdates
     else{
       tx('Logging')->log('Update', 'Updating DB', 'No method '.$method.' for package '.$this->package()->title.' from version '.$this->current_version());
       $this->version_bump($next);
+      $this->update($forced, $maybe_install);
       return true;
     }
     
@@ -379,7 +381,8 @@ abstract class BaseDBUpdates
   
   /* ---------- Protected ---------- */
   
-  protected function get_base_dir(){
+  protected function get_base_dir()
+  {
     
     if($this->is_core === true)
       return self::base_dir('core');
