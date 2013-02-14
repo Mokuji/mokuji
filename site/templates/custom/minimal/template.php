47cfa496a28de4c5b164d978f655a343334ec775
Bart Roorda <bart@tuxion.nl>
2013-02-14 08:34:19 +0100

    Updated thema and template: minimal. It's minimal now.

--- site/templates/custom/minimal/template.php
+++ site/templates/custom/minimal/template.php
@@ -4,15 +4,11 @@
 
   <head>
   
-    <title><?php echo $head->title; ?></title>
     <base href="<?php echo $head->base->href; ?>" target="<?php echo $head->base->target; ?>" />
 
-    <!-- character encoding -->
     <meta http-equiv="Content-Type" content="text/html;charset=UTF-8" />
-
-    <!-- robots -->
-    <meta name="robots" content="noindex, nofollow" />
     <meta name="revisit-after" content="15 days" />
+
     <?php echo $head->meta; ?>
     
     <?php echo $head->links; ?>
@@ -26,23 +22,8 @@
   </head>
   
   <body>
-  
-    <div id="error">
-      <?php echo $messages->error; ?>
-    </div>
-    
-    <div id="notification">
-      <?php echo $messages->notification; ?>
-    </div>
-    
-    <div id="menu">
-       <?php echo tx('Component')->modules('menu')->get_html('menu'); ?>
-    </div>
-    
-    <div id="content">
-      <?php echo $body->content; ?>
-    </div>
-  
+
+    <?php echo $body->content; ?>
     <?php echo $body->admin_toolbar; ?>
   
   </body>
