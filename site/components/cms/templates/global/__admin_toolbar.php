f64a6cdc8f2dd461111f08bc5bfcca0d1b4ff85e
Beanow <beanow@project-virality.com>
2013-05-17 17:09:01 +0200

    Updated translations.

--- site/components/cms/templates/global/__admin_toolbar.php
+++ site/components/cms/templates/global/__admin_toolbar.php
@@ -32,8 +32,8 @@
   <?php if(tx('Account')->check_level(2)): ?>
     <li class="website<?php if(!EDITABLE) echo ' active'; ?>"><a href="<?php echo $admin_toolbar->website_url; ?>"><?php __('Website'); ?></a></li>
     <li class="website-edit<?php if(EDITABLE) echo ' active'; ?>"><a href="<?php echo $admin_toolbar->edit_url; ?>">Edit</a></li>
-    <li class="advanced-edit"><a title="<?php __($names->component, 'Edit the current webpage'); ?>" href="<?php echo $admin_toolbar->advanced_url; ?>"><?php __('Advanced editing'); ?></a></li>
-    <li class="advanced"><a title="<?php __($names->component, 'Manage the website'); ?>" href="<?php echo $admin_toolbar->admin_url; ?>"><?php __('Control Panel'); ?></a></li>
+    <li class="advanced-edit"><a title="<?php __($names->component, 'Edit the current webpage'); ?>" href="<?php echo $admin_toolbar->advanced_url; ?>"><?php __($names->component, 'Advanced editing'); ?></a></li>
+    <li class="advanced"><a title="<?php __($names->component, 'Manage the website'); ?>" href="<?php echo $admin_toolbar->admin_url; ?>"><?php __($names->component, 'Control Panel'); ?></a></li>
   <?php endif; ?>
 
 </ul>
