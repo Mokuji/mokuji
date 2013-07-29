8bceff770b3a90729c08cfa60f7c1f5473e483bb
Beanow <beanow@project-virality.com>
2013-07-29 18:55:52 +0200

    Added jQuery plugin to show password improvement tips.

---
+++ plugins/jquery_password_strength/i18n/nl-NL.js
@@ -0,0 +1,25 @@
+//The nl-NL locale.
+(function($){ $.fn.PasswordStrength.i18n = {
+  
+  //Strength names.
+  strength_very_weak:   "Zeer zwak",
+  strength_weak:        "Zwak",
+  strength_decent:      "Degelijk",
+  strength_strong:      "Sterk",
+  strength_very_strong: "Zeer sterk",
+  
+  //The tips per password strength and bias.
+  tip_none:             "Onthoud dat je wachtwoorden niet moet hergebruiken. Wachtwoordbeheerders kunnen daarbij helpen.",
+  tip_very_weak:        "Het wachtwoord is te kort. Langere wachtwoorden zijn veiliger, probeer om willekeurige woorden toe te voegen.",
+  tip_biased_weak:      "Langere wachtwoorden zijn veiliger, probeer om willekeurige woorden in hoofdletters en kleine letters toe te voegen.",
+  tip_biased_decent:    "Maar je kan vast nog beter! Probeer het nog langer te maken of symbolen er in te mengen.",
+  tip_biased_strong:    "Je doet duidelijk goed je best! Als je de best mogelijke wachtwoorden wilt hebben, probeer dan een wachtwoordbeheersysteem om unieke wachtwoorden te genereren voor iedere dienst.",
+  tip_balanced_weak:    "Voor meer veiligheid, probeer het langer te maken door extra woorden toe te voegen.",
+  tip_balanced_decent:  "Maar je kan vast nog beter! Probeer het nog langer te maken door extra woorden toe te voegen.",
+  tip_balanced_strong:  "Maar onthoud dat je wachtwoorden niet moet hergebruiken. Wachtwoordbeheerders kunnen daarbij helpen.",
+  tip_uncommon_weak:    "Waarschijnlijk doe je het fout, dit is lastig te onthouden en helemaal niet veilig (http://xkcd.com/936). Probeer nieuwe woorden toe te voegen om het langer te maken.",
+  tip_uncommon_decent:  "Maar waarschijnlijk doe je het fout, dit is lastig te onthouden en helemaal niet veilig (http://xkcd.com/936). Probeer nieuwe woorden toe te voegen om het langer te maken.",
+  tip_uncommon_strong:  "Maar onthoud dat je wachtwoorden niet moet hergebruiken. Wachtwoordbeheerders kunnen daarbij helpen.",
+  tip_very_strong:      "Maar onthoud dat je wachtwoorden niet moet hergebruiken. Wachtwoordbeheerders kunnen daarbij helpen."
+  
+}; })(jQuery);
