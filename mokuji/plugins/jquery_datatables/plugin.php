17ece19887575e63fbe7642463a23a98f6adabd0
Beanow <beanow@project-virality.com>
2014-01-30 18:49:29 +0100

    Fixed language file problem with jquery_datatables plugin.

--- mokuji/plugins/jquery_datatables/plugin.php
+++ mokuji/plugins/jquery_datatables/plugin.php
@@ -3,15 +3,30 @@
   <link type="text/css" rel="stylesheet" href="<?php echo $plugin; ?>media/css/jquery.dataTables_tuxion.css" media="screen" charset="utf-8">
 
   <script type="text/javascript" language="javascript" src="<?php echo $plugin ?>media/js/jquery.dataTables.js"></script>
-
-  <script>
-            
-  $(document).ready(function() {
-    $.extend( $.fn.dataTable.defaults, {
-      "oLanguage": {
-        "sUrl": "<?php echo $plugin; ?>media/language/<?php echo tx('Language')->code; ?>.json"
-      }
-    });
-  });
   
-  </script>
+  <?php
+  
+  $i18n_file = $plugin.'media/language/'.mk('Language')->code.'.json';
+  if(file_exists($i18n_file))
+  {
+    
+    ?>
+    
+    <script>
+              
+    $(document).ready(function() {
+      $.extend( $.fn.dataTable.defaults, {
+        "oLanguage": {
+          "sUrl": "<?php echo $i18n_file ?>"
+        }
+      });
+    });
+    
+    </script>
+    
+    <?php
+    
+  }
+    
+  ?>
+  
