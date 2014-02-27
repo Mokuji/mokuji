a220fe426fc1e27876edd2d798ef4141227aa094
Beanow <beanow@project-virality.com>
2014-02-27 16:35:15 +0100

    Added alternative URL validator.

--- mokuji/system/dependencies/Validator.php
+++ mokuji/system/dependencies/Validator.php
@@ -479,7 +479,7 @@ class Validator extends Successable
     
   }
   
-  private function _url()
+  private function _url($type=null)
   {
     
     $raw_data = data_of($this->data);
@@ -490,36 +490,52 @@ class Validator extends Successable
     
     try{
       
-      $url = $this->data;
-      
-      if(strpos($this->data, '://') === false)
-        $url = 'http://'.$url;
-      
-      $segments = tx('Url')->parse($url);
-      
-      //Must contain a domain at the very least.
-      if(!array_key_exists('domain', $segments))
-        return $this->ctransf('The value must be a url.');
-      
-      //See if it's an IP.
-      $is_ip = !!filter_var($segments['domain'], FILTER_VALIDATE_IP);
-      
-      //If it's not an IP, check the domain.
-      if(!$is_ip){
+      if($type == 'web'){
         
-        //It also can't be localhost, so must have a dot.
-        if(strpos($segments['domain'], '.') === false)
-          return $this->ctransf('The value must be a url.');
+        //From https://gist.github.com/gruber/8891611
+        $success = preg_match(
+          "~(?i)\b((?:https?:(?:/{1,3}|[a-z0-9%])|[a-z0-9.\-]+[.](?:com|net|org|edu|gov|mil|aero|asia|biz|cat|coop|info|int|jobs|mobi|museum|name|post|pro|tel|travel|xxx|ac|ad|ae|af|ag|ai|al|am|an|ao|aq|ar|as|at|au|aw|ax|az|ba|bb|bd|be|bf|bg|bh|bi|bj|bm|bn|bo|br|bs|bt|bv|bw|by|bz|ca|cc|cd|cf|cg|ch|ci|ck|cl|cm|cn|co|cr|cs|cu|cv|cx|cy|cz|dd|de|dj|dk|dm|do|dz|ec|ee|eg|eh|er|es|et|eu|fi|fj|fk|fm|fo|fr|ga|gb|gd|ge|gf|gg|gh|gi|gl|gm|gn|gp|gq|gr|gs|gt|gu|gw|gy|hk|hm|hn|hr|ht|hu|id|ie|il|im|in|io|iq|ir|is|it|je|jm|jo|jp|ke|kg|kh|ki|km|kn|kp|kr|kw|ky|kz|la|lb|lc|li|lk|lr|ls|lt|lu|lv|ly|ma|mc|md|me|mg|mh|mk|ml|mm|mn|mo|mp|mq|mr|ms|mt|mu|mv|mw|mx|my|mz|na|nc|ne|nf|ng|ni|nl|no|np|nr|nu|nz|om|pa|pe|pf|pg|ph|pk|pl|pm|pn|pr|ps|pt|pw|py|qa|re|ro|rs|ru|rw|sa|sb|sc|sd|se|sg|sh|si|sj|Ja|sk|sl|sm|sn|so|sr|ss|st|su|sv|sx|sy|sz|tc|td|tf|tg|th|tj|tk|tl|tm|tn|to|tp|tr|tt|tv|tw|tz|ua|ug|uk|us|uy|uz|va|vc|ve|vg|vi|vn|vu|wf|ws|ye|yt|yu|za|zm|zw)/)(?:[^\s()<>{}\[\]]+|\([^\s()]*?\([^\s()]+\)[^\s()]*?\)|\([^\s]+?\))+(?:\([^\s()]*?\([^\s()]+\)[^\s()]*?\)|\([^\s]+?\)|[^\s`!()\[\]{};:'\".,<>?«»“”‘’])|(?:(?<!@)[a-z0-9]+(?:[.\-][a-z0-9]+)*[.](?:com|net|org|edu|gov|mil|aero|asia|biz|cat|coop|info|int|jobs|mobi|museum|name|post|pro|tel|travel|xxx|ac|ad|ae|af|ag|ai|al|am|an|ao|aq|ar|as|at|au|aw|ax|az|ba|bb|bd|be|bf|bg|bh|bi|bj|bm|bn|bo|br|bs|bt|bv|bw|by|bz|ca|cc|cd|cf|cg|ch|ci|ck|cl|cm|cn|co|cr|cs|cu|cv|cx|cy|cz|dd|de|dj|dk|dm|do|dz|ec|ee|eg|eh|er|es|et|eu|fi|fj|fk|fm|fo|fr|ga|gb|gd|ge|gf|gg|gh|gi|gl|gm|gn|gp|gq|gr|gs|gt|gu|gw|gy|hk|hm|hn|hr|ht|hu|id|ie|il|im|in|io|iq|ir|is|it|je|jm|jo|jp|ke|kg|kh|ki|km|kn|kp|kr|kw|ky|kz|la|lb|lc|li|lk|lr|ls|lt|lu|lv|ly|ma|mc|md|me|mg|mh|mk|ml|mm|mn|mo|mp|mq|mr|ms|mt|mu|mv|mw|mx|my|mz|na|nc|ne|nf|ng|ni|nl|no|np|nr|nu|nz|om|pa|pe|pf|pg|ph|pk|pl|pm|pn|pr|ps|pt|pw|py|qa|re|ro|rs|ru|rw|sa|sb|sc|sd|se|sg|sh|si|sj|Ja|sk|sl|sm|sn|so|sr|ss|st|su|sv|sx|sy|sz|tc|td|tf|tg|th|tj|tk|tl|tm|tn|to|tp|tr|tt|tv|tw|tz|ua|ug|uk|us|uy|uz|va|vc|ve|vg|vi|vn|vu|wf|ws|ye|yt|yu|za|zm|zw)\b/?(?!@)))~",
+          $raw_data
+        ) !== false;
         
-        //The domain must have a valid top level domain.
-        $domainparts = explode('.', $segments['domain']);
-        $tld = end($domainparts);
-        if(!in_array($tld, \core\Url::$TOP_LEVEL_DOMAINS))
-          return $this->ctransf('The value must be a url.');
+        if(!$success) return $this->ctransf('The value must be a url.');
         
       }
       
-      $this->data = url($url)->output;
+      //Not type = web
+      else{
+        $url = $this->data;
+        
+        if(strpos($this->data, '://') === false)
+          $url = 'http://'.$url;
+        
+        $segments = tx('Url')->parse($url);
+        
+        //Must contain a domain at the very least.
+        if(!array_key_exists('domain', $segments))
+          return $this->ctransf('The value must be a url.');
+        
+        //See if it's an IP.
+        $is_ip = !!filter_var($segments['domain'], FILTER_VALIDATE_IP);
+        
+        //If it's not an IP, check the domain.
+        if(!$is_ip){
+          
+          //It also can't be localhost, so must have a dot.
+          if(strpos($segments['domain'], '.') === false)
+            return $this->ctransf('The value must be a url.');
+          
+          //The domain must have a valid top level domain.
+          $domainparts = explode('.', $segments['domain']);
+          $tld = end($domainparts);
+          if(!in_array($tld, \core\Url::$TOP_LEVEL_DOMAINS))
+            return $this->ctransf('The value must be a url.');
+          
+        }
+        
+        $this->data = url($url)->output;
+        
+      } // end: Not type = web
       
     } catch (\exception\Unexpected $ue) {
       return $this->ctransf('The value must be a url.');
