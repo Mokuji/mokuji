a6a0b395228df69ede5609ad80d81677cad58025
Beanow <beanow@project-virality.com>
2014-04-03 15:05:55 +0200

    Adding basic fallback for email messages.

--- mokuji/system/dependencies/email/EmailTasks.php
+++ mokuji/system/dependencies/email/EmailTasks.php
@@ -36,14 +36,98 @@ abstract class EmailTasks
     //Apply the format we picked.
     $params['html_message'] = EmailFormatting::format($template['type'], $output);
     
-    #TODO: Support fallback methods.
-    //Send email.
-    mk('Component')->helpers('mail')->send_fleeting_mail($params)
+    //Supporting mail component for advanced delivery.
+    if(mk('Component')->available('mail'))
+    {
+      
+      
+      //Send email.
+      mk('Component')->helpers('mail')->send_fleeting_mail($params)
+      
+      ->failure(function($info){
+        mk('Logging')->log('Email', 'Error sending', $info->get_user_message());
+        throw new \exception\Unexpected($info->get_user_message());
+      }); 
+      
+    }
     
-    ->failure(function($info){
-      mk('Logging')->log('Email', 'Error sending', $info->get_user_message());
-      throw new \exception\Unexpected($info->get_user_message());
-    }); 
+    else
+    {
+      
+      //A formatter that can handle most of the Mail component formats.
+      $quickformat = function($addresses){
+        
+        //Strings, we don't format.
+        if(is_string($addresses)){
+          return $addresses;
+        }
+        
+        //Arrays may be of two types.
+        if(is_array($addresses)){
+          
+          //Single address, with kv pairs.
+          if(array_key_exists('email', $addresses))
+            return $addresses['email'];
+          
+          //Otherwise it's a set.
+          $formattedAddresses = array();
+          
+          foreach($addresses as $address){
+            
+            //If it's a name & email key value pair, format it.
+            if(array_key_exists('email', $address)){
+              $formattedAddresses[] = $address['email'];
+            }
+            
+            //Array of strings, can be imploded.
+            elseif(is_string($address)){
+              $formattedAddresses[] = $address;
+            }
+            
+            else
+              throw new \exception\Unexpected('Unknown email formatting');
+            
+          }
+          
+          //Now add the comma's.
+          return implode(',', $formattedAddresses);
+          
+        }
+        
+        throw new \exception\Unexpected('Unknown email formatting');
+        
+      };
+      
+      //Additional headers
+      $headers  = 'X-Mailer: Mokuji EmailTasks'.n;
+      $headers .= 'From: '.$quickformat(
+        isset($messageData['from']) ?
+          $messageData['from']:
+          EMAIL_NAME_AUTOMATED_MESSAGES.' <'.EMAIL_ADDRESS_AUTOMATED_MESSAGES.'>'
+      ).n;
+      
+      if(isset($messageData['cc'])){
+        $headers .= 'Cc: '.$quickformat($messageData['cc']).n;
+      }
+      
+      if(isset($messageData['bcc'])){
+        $headers .= 'Bcc: '.$quickformat($messageData['bcc']).n;
+      }
+      
+      //Try send it.
+      $success = @mail(
+        $quickformat($messageData['to']),
+        $messageData['subject'],
+        strip_tags($params['html_message']),
+        $headers
+      );
+      
+      if(!$success){
+        mk('Logging')->log('Email', 'Error sending', 'Mail function reported unsuccessful queue.');
+        throw new \exception\Unexpected('Mail function reported unsuccessful queue.');
+      }
+      
+    }
     
     mk('Logging')->log('Email', 'Sent', $messageKey);
     return true;
